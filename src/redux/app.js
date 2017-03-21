f0f57a5932649ec4af1d872efccbbe522931d133
Chitanda60 <freeyinyutian@gmail.com>
2017-03-21 15:51:44 +0800

    日常提交

--- src/redux/app.js
+++ src/redux/app.js
@@ -1,7 +1,11 @@
 
 import React, {Component} from 'react'
 import {render} from 'react-dom'
-import {createStore, combineReducers} from 'redux'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
+import createLogger from 'redux-logger'
+import thunk from 'redux-thunk'
+import promiseMiddleware from 'redux-promise'
+import {createAction} from 'redux-actions'
 
 // type
 const ADD_TODO = 'ADD_TODO'
@@ -9,17 +13,36 @@ const DES_TODO = 'DES_TODO'
 const SET_NAME = 'SET_NAME'
 
 // action
+const fetchData = new Promise((resolve, reject) => {
+	if (true) {
+		setTimeout(() => resolve({data: 'RESOLVE'}), 3000)
+	} else {
+		setTimeout(() => reject({data: 'REJECT'}), 3000)
+	}
+})
+
 const action = {
-	addTodo: () => {
-		return {
-			type: 'ADD_TODO'			
-		}
+	addTodo: () => (dispatch, getState) => { 
+		dispatch({ type: 'ADD_TODO' }) 
 	},
 	desTodo: () => {
 		return {
 			type: 'DES_TODO'
 		}
-	}	
+	},
+	showTodo: () => {
+		// promiseMiddleware 用法1 action不是标准的flux action 然后检查payload是否是promise
+		return fetchData.then((res) => {			
+			return {
+				type: 'SHOW_TODO',
+				preload: res.data
+			}
+		})
+	},
+	hideTodo: () => {
+		// promiseMiddleware 用法2 通过createAction创建标准的flux action ，然后检查payload是否是promise
+		return createAction('HIDE_TODO')(fetchData.then(res => res.data))
+	}
 }
 
 // reducer
@@ -30,6 +53,12 @@ const calcReducer = (state = 1, action) => {
 
 		case 'DES_TODO':
 			return state - 1
+
+		case 'SHOW_TODO':
+			return state
+
+		case 'HIDE_TODO':
+			return state
 
 		default:
 			return state
@@ -49,48 +78,72 @@ const Reducer = combineReducers({
 })
 
 // store
-const store = createStore(Reducer)
+const store = createStore(
+	Reducer,
+	applyMiddleware(
+		thunk, 
+		promiseMiddleware,
+		createLogger()
+	)
+)
 
 // container
 class Counter extends Component {
 	constructor(props) {
-		super(props)		
+		super(props)
 	}
 
 	onAdd() {
+		// 返回一个thunk
 		console.log('ADD_TODO')
-		store.dispatch({type: 'ADD_TODO'})
-	}
-	onDes() {
-		console.log('DES_TODO')
-		store.dispatch({type: 'DES_TODO'})
+		store.dispatch(action.addTodo())
 	}
 
-	render() {		
+	onDes() {
+		// 返回一个action
+		console.log('DES_TODO')
+		store.dispatch(action.desTodo())
+	}
+
+	onShow() {
+		// 返回一个promise或者Action的payload属性是promise
+		console.log('SHOW_TODO')
+		store.dispatch(action.showTodo())
+	}
+
+	onHide() {
+		console.log('HIDE_TODO')
+		store.dispatch(action.hideTodo())
+	}
+
+	render() {
 		const {calcReducer} = store.getState()
-		return <CounterItem value={calcReducer} onAdd={this.onAdd} onDes={this.onDes} />
+		return <CounterItem value={calcReducer} onAdd={this.onAdd} onDes={this.onDes} onShow={this.onShow} onHide={this.onHide} />
 	}
 }
 
 // component
-const CounterItem = ({value, onAdd, onDes}) => (
+const CounterItem = ({value, onAdd, onDes, onShow, onHide}) => (
 	<div>
 		<h1>{value}</h1>
 		<div className="add" onClick={onAdd}>ADD</div>
 		<div className="dec" onClick={onDes}>DES</div>
+		<div className="show" onClick={onShow}>SHOW</div>
+		<div className="hide" onClick={onHide}>HIDE</div>
 	</div>
 )
 
 // render
 const Render = () => {
-		render(
-			<Counter />,
-			document.getElementById('root')
-		)
+	render(
+		<Counter />,
+		document.getElementById('root')
+	)
 }
 Render()
 store.subscribe(Render)
 
+// 部分源码实现
 // const createStore = (reducer) => {
 // 	let state
 // 	let listeners = []
@@ -110,10 +163,49 @@ store.subscribe(Render)
 // 	}
 // }
 
+// const applyMiddleware = (...middlewares) => {
+// 	return (createStore) => (reducer, preloadedState, enhancer) => {
+// 		//createStore时调用enhancer是传入参数
+// 		var store = createStore(reducer, preloadedState, enhancer)
+// 		var dispatch = store.dispatch
+// 		var chain = []
+// 		var middlewareAPI = {
+// 			getState: store.getState,
+// 			dispatch: (action) => dispatch(action)
+// 		}
+// 		chain = middlewares.map(middleware => middleware(middlewareAPI))
+//		// 从右到左逐层包装dispatch
+// 		dispatch = compose(...chain)(store.dispatch)
+// 		return {...store, dispatch}
+// 	}
+// }
 
+// const compose = (...funcs) => {
+// 	if (funcs.length === 0) {
+// 		return arg => arg
+// 	}
+// 	if (funcs.lenght === 1) {
+// 		return funcs[0]
+// 	}
+// 	const last = funcs[funcs.length - 1]
+// 	const rest = funcs.slice(0, -1)
+// 	//reduceRight是为了让next作为前一个middleware的参数
+// 	return (...args) => rest.reduceRight((composed, f) => f(composed), last(...args))
+// }
 
-
-
+// const createThunkMiddleware = (extraArgument) => {
+// 	// {dispatch, getState} 在applyMiddleware中传入
+// 	// next 在compose中传入，就是下一个middleware
+// 	// action 在dispatch传入
+// 	return ({dispatch, getState}) => next => action => {
+// 		if (typeof action === 'function') {
+// 			return action(dispatch, getState, extraArgument)
+// 		}
+// 		return next(action)
+// 	}
+// }
+// const thunk = createThunkMiddleware()
+// thunk.withExtraArgument = createThunkMiddleware
 
 
 
