c88620caf0572105246adb4b1c7232f46a220f1e
Chitanda60 <freeyinyutian@gmail.com>
2017-03-24 16:28:09 +0800

    日常提交

--- src/redux/app3.js
+++ src/redux/app3.js
@@ -22,19 +22,23 @@ render(
 )
 
 // history.push -> store.dispatch -> enhancedHistory.listen -> router
-const defaultSelectLocationState = state => state.routing
-const syncHistoryWithStore = (history, store, {selectLocationState = defaultSelectLocationState} = {}) => {
-	const getLocationInStore = () => {
-		const locationState = selectLocationState(store.getState())
-	}
+// const defaultSelectLocationState = state => state.routing
+// const syncHistoryWithStore = (history, store, {selectLocationState = defaultSelectLocationState} = {}) => {
+// 	const getLocationInStore = () => {
+// 		const locationState = selectLocationState(store.getState())
+// 	}
 
-	const handleStoreChange = () => {
-		const locationInStore = getLocationInStore(true)
+// 	const handleStoreChange = () => {
+// 		const locationInStore = getLocationInStore(true)
 
-	}
+// 	}
 
-	let unsubscribeFromStore = store.subscribe(handleStoreChange)
-}
+// 	let unsubscribeFromStore = store.subscribe(handleStoreChange)
+// }
+
+
+
+
 
 // react-router
 // Router.init = () => {
@@ -42,7 +46,7 @@ const syncHistoryWithStore = (history, store, {selectLocationState = defaultSele
 // 	window.addEventListener('hashchange', this.refresh.bind(this), false)
 // }
 
-// 路由核心模块 收集listener&执行listener
+// 路由核心模块 收集listener & 执行listener
 // const history = {
 // 	listeners: [],
 // 	listen: (listener) => {
@@ -70,6 +74,55 @@ const syncHistoryWithStore = (history, store, {selectLocationState = defaultSele
 // 	return {path, component}
 // }
 // Router(history, [Route('/user', 'UserPageComponent')])
+
+const Router = {
+	componentWillMount: () => {
+		const history = this.props.history
+		// 包装history得到_useRoutes2的listen方法
+		this.history = _useRoutes2['default'](history)({ routes: routes, parseQueryString: parseQueryString, stringifyQuery: stringifyQuery })
+		// 为Router注册一个含有setState的回调函数 用以触发render
+		this.unlisten = this.history.listen((error, state) => _this.setState(state, _this.props.onUpdate))
+		// 添加到ChangeListeners
+		const listen = (listener) => {
+			// ChangeListeners.push(listener)
+			return history.listen((location) => {
+				// 根据当前location和路由表匹配到对应的路由子集 得到对应的state执行回调展示更新
+				match(location, (error, redirectLocation, nextState) => {
+					listener(null, nextState)
+				})
+			})
+		}
+	},
+}
+const Link = {
+	render: () => {
+		props.onClick = (e) =>  {
+			return _this.handleClick(e)
+		}
+		if (history) {
+			// 将参数组合成href属性
+			props.href = history.createHref(to, query)
+		}
+		// 返回a标签
+		return _react2['default'].createElement('a', props)
+	},
+	handleClick = (event) => {
+		event.preventDefault()
+		// Link实质上是执行pushState 具体更新url方法分为两种
+		// createBrowserHistory 使用 window.history.replaceState(historyState, null, path)
+		// createHashHistory 使用 window.location.hash = url
+		this.context.history.pushState(this.props.state, this.props.to, this.props.query)
+		// 更新url之后调用updateLocation
+	}
+}
+
+// 路由跳转分两步 更新路由:各种形式的push + 更新展示:触发监听，即调用updateLocation 执行监听回调触发
+const updateLocation = (newLocation) => {
+	ChangeListeners.forEach((listener) => {
+		listener(newLocation)
+	})
+}
+
 
 
 
